1a093b63fbdc5ac8f20ddfeb2c5b74e484740308
Ryan Smith <smithrp@gmail.com>
2015-04-27 19:17:12 -0700

    new documetation on feedback object

--- src/main/java/io/swagger/client/model/Feedback.java
+++ src/main/java/io/swagger/client/model/Feedback.java
@@ -9,6 +9,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Feedback  {
   
   private String apiKey = null;
+  private String searchKey = null;
+  private String drugId = null;
+  private String reason = null;
+  private String message = null;
+  private String url = null;
 
   
   /**
@@ -24,6 +29,71 @@ public class Feedback  {
   }
 
   
+  /**
+   * the search key that was provided while reporting the feedback
+   **/
+  @ApiModelProperty(value = "the search key that was provided while reporting the feedback")
+  @JsonProperty("searchKey")
+  public String getSearchKey() {
+    return searchKey;
+  }
+  public void setSearchKey(String searchKey) {
+    this.searchKey = searchKey;
+  }
+
+  
+  /**
+   * the id of the drug that was being reported
+   **/
+  @ApiModelProperty(value = "the id of the drug that was being reported")
+  @JsonProperty("drugId")
+  public String getDrugId() {
+    return drugId;
+  }
+  public void setDrugId(String drugId) {
+    this.drugId = drugId;
+  }
+
+  
+  /**
+   * pre defined reason for categorizing the problem [Other, Currently Closed, Irrelevant, Out of Order - Move Down, Out of Order - Move Up, Wrong Mutation, Wrong Tumor Type]
+   **/
+  @ApiModelProperty(value = "pre defined reason for categorizing the problem [Other, Currently Closed, Irrelevant, Out of Order - Move Down, Out of Order - Move Up, Wrong Mutation, Wrong Tumor Type]")
+  @JsonProperty("reason")
+  public String getReason() {
+    return reason;
+  }
+  public void setReason(String reason) {
+    this.reason = reason;
+  }
+
+  
+  /**
+   * detailed message for the incident being created
+   **/
+  @ApiModelProperty(value = "detailed message for the incident being created")
+  @JsonProperty("message")
+  public String getMessage() {
+    return message;
+  }
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  
+  /**
+   * url to reproduce the search results
+   **/
+  @ApiModelProperty(value = "url to reproduce the search results")
+  @JsonProperty("url")
+  public String getUrl() {
+    return url;
+  }
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  
 
   @Override
   public String toString()  {
@@ -31,6 +101,11 @@ public class Feedback  {
     sb.append("class Feedback {\n");
     
     sb.append("  apiKey: ").append(apiKey).append("\n");
+    sb.append("  searchKey: ").append(searchKey).append("\n");
+    sb.append("  drugId: ").append(drugId).append("\n");
+    sb.append("  reason: ").append(reason).append("\n");
+    sb.append("  message: ").append(message).append("\n");
+    sb.append("  url: ").append(url).append("\n");
     sb.append("}\n");
     return sb.toString();
   }
